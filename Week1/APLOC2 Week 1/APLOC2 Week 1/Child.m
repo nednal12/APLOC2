6e69a11ce02f04006062caf1362a791493b3d6f3
Brent Marohnic <nednal12@fullsail.edu>
2013-02-07 05:05:46 -0500

    Added New Class

---
+++ Week1/APLOC2 Week 1/APLOC2 Week 1/Child.m
@@ -0,0 +1,13 @@
+//
+//  Child.m
+//  APLOC2 Week 1
+//
+//  Created by Brent Marohnic on 2/7/13.
+//  Copyright (c) 2013 Brent Marohnic. All rights reserved.
+//
+
+#import "Child.h"
+
+@implementation Child
+
+@end
